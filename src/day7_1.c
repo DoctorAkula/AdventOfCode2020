39d6edcddc08df3c354e8609e300866d30545f39
Christopher Wagner <Chris@Cooler.wagner-x.net>
2020-12-13 16:11:32 -0800

    fix day7_1.c

--- src/day7_1.c
+++ src/day7_1.c
@@ -12,20 +12,52 @@ char *delimBagLines(char **input)
 	*input = next + 1;
 	return group;
 }
-int countBags(char *input, char *bagname) 
+void countBags(char *input, char *bagname, int bagCount, bool *containGold) 
 {
 	char *dupein = input;
-	char *rules = delimBagLines(&dupein);
-	char *containName;
-	printf("<%s>\n\n", rules);
-	free(rules);
+	for(int i = 0; i < bagCount; i++)
+	{
+		char *rules = delimBagLines(&dupein);
+		if(!rules)
+			return;
+		char *next = strstr(rules, " contain");
+		if(!next)
+			return;
+		unsigned long long containlen = (unsigned long long)(next - rules);
+		char *containName = calloc(containlen, sizeof(char));
+		strncpy(containName, rules, containlen);
+		if(containName[containlen-1] == 's')
+			containName[containlen-1] = '\0';
+		else
+			containName[containlen] = '\0';
+
+		if(strstr(next, bagname))
+		{
+		//	printf("name:%s\n<%s>\n\n", containName, next);
+			containGold[i] = true;
+			countBags(input, containName, bagCount, containGold);
+		}
+
+		free(containName);
+		free(rules);
+	}
 }
 void day7_1(){
-	char *input = loadinput("../input/test6.txt");
+	char *input = loadinput("../input/day7-1.txt");
 	int i = 0;
-	bagCount = 0;
+	int bagCount = 0;
 	while(input[i] != '\0')
 		if(input[i++] == '\n') bagCount++;
 	bool *containGold = calloc(bagCount, sizeof(bool));
+	printf("%d\n", bagCount);
+	countBags(input, "shiny gold bag", bagCount, containGold);
+	int count = 0;
+	for(i  = 0; i < bagCount; i++)
+	{
+		printf("%d", containGold[i]);
+		if(containGold[i]) count++;
+	}
+	printf("\nBag Containers: %d\n", count);
+	free(containGold);
 	free(input);
 }
